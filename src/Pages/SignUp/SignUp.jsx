dd2b5ed0be7adf3c1915ec6f1fe70249f0e8f4ed
kamelmahmud0408 <mahmudulhasankamel1994@gmail.com>
2023-05-18 13:53:57 -0700

    added sign up page

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Login = () => {
+const SignUp = () => {
     return (
         <div className="hero min-h-screen ">
             <div className="hero-content flex-col lg:flex-row">
@@ -9,9 +9,17 @@ const Login = () => {
                     <img className='w-full' src="https://img.freepik.com/free-vector/sign-concept-illustration_114360-125.jpg?w=2000" alt="" />
                 </div>
                 <div className="card flex-shrink-0  shadow-xl bg-base-100 w-1/3  mx-auto p-5">
-                    <h1 className="text-5xl my-3 text-center font-bold">Please Login</h1>
+                    <h1 className="text-5xl my-3 text-center font-bold">Please Sign up</h1>
                     <form  >
-                        <div>
+                        <div className='mt-4'>
+                            <label htmlFor="name">Name</label> <br />
+                            <input className='border w-full p-3 rounded  mt-4' type="text" name="displayName" id="" placeholder='Your Name' required />
+                        </div>
+                        <div className='mt-4'>
+                            <label htmlFor="photo">Photo URL</label> <br />
+                            <input className='border w-full p-3 rounded  mt-4' type="text" name="photo" id="" placeholder='Your Photo URL' required />
+                        </div>
+                        <div className='mt-4'>
                             <label htmlFor="">Email</label> <br />
                             <input className='border w-full p-3 rounded mt-4' type="email" name="email" id="" placeholder='Your Email' required />
                         </div>
@@ -24,9 +32,9 @@ const Login = () => {
                     </form>
                     <div className="divider">OR</div>
                     <div>
-                         <p className='btn btn-block text-white bg-orange-500 '>Google</p>
+                        <p className='btn btn-block text-white bg-orange-500 '>Google</p>
                     </div>
-                    <p className='mt-5 text-center'><span>  Don't Have an Account? <Link className='text-blue-600' to="/signup">Register</Link></span></p>
+                    <p className='mt-5 text-center'><span>  Already have an account ? Please <Link className='text-blue-600' to="/login">login</Link></span></p>
                 </div>
             </div>
         </div>
@@ -34,4 +42,4 @@ const Login = () => {
     );
 };
 
-export default Login;
+export default SignUp;
