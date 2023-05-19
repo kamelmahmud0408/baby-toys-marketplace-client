42e5c9230c6031b1c691f3d102c526e04bcb6039
kamelmahmud0408 <mahmudulhasankamel1994@gmail.com>
2023-05-19 06:52:36 -0700

    added sub category section and onclick added

--- src/Pages/Home/CarsCategory/CarsCategory.jsx
+++ src/Pages/Home/CarsCategory/CarsCategory.jsx
@@ -1,30 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 
 const CarsCategory = () => {
-    return (
-        <div>
-            <Tabs>
-                
-                <div className='text-center list-style-none'>
-                    <Tab className={`text-red-700 text-3xl font-bold  `} >Reguler Cars</Tab>
-                    <Tab className={`text-red-700`}>Pulice cars</Tab>
-                    <Tab className={`text-red-700`}>truck</Tab>
-                </div>
 
+    const [toys,setToys]=useState([])
+    const [activeTab,setActiveTab]=useState('')
+
+    const handleClick=(categoryName)=>{
+                  setActiveTab(categoryName)
+    }
+    return (
+        <div className='text-center text-xl font-bold'>
+            <Tabs>
+                <TabList>
+                    <Tab onClick={()=>handleClick('RegularCar')}>Regular car</Tab>
+                    <Tab onClick={()=>handleClick('PuliceCar')}>Pulice Car</Tab>
+                    <Tab onClick={()=>handleClick('Truck')}>Truck</Tab>
+                </TabList>
 
                 <TabPanel>
                     <h2>Any content 1</h2>
+                    {
+                        console.log(activeTab)
+                    }
                 </TabPanel>
                 <TabPanel>
                     <h2>Any content 2</h2>
                 </TabPanel>
                 <TabPanel>
-                    <h2>Any content 2</h2>
+                    <h2>Any content 3</h2>
                 </TabPanel>
             </Tabs>
-
         </div>
     );
 };
